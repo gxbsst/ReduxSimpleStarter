55aa545c856744b7625dfcdbd6f1e793cfc68305
gxbsst <gxbsst@gmail.com>
2016-06-11 22:01:41 +0800

    增加video detail

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,22 @@
 /// <reference path="../../typings/index.d.ts" />
- import React, {Component} from 'react';
- 
- const VideoListItem = (props)  => {
-   return <li>hahh </li>;
- }
- 
- export default VideoListItem;
+import React, {Component} from 'react';
+
+const VideoListItem = ({video}) => {
+  console.log(video)
+  const imageUrl = video.snippet.thumbnails.default.url
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading"> {video.snippet.title} </div>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default VideoListItem;
