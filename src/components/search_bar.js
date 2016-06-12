bfc00aca664be8d09d8cab0032e8edef9cff404a
gxbsst <gxbsst@gmail.com>
2016-06-12 15:53:17 +0800

    增加样式css

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,11 +13,10 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={ event => this.setState({ term: event.target.value }) }/>
-        value of the input: {this.state.term}
       </div>
     );
   }
