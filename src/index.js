d8d85b19119b15f9092229e860207a79555a9b32
gxbsst <gxbsst@gmail.com>
2016-06-12 16:41:05 +0800

    搜索延迟300ms相应

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 /// <reference path="../typings/index.d.ts" />
+import _ from 'lodash'
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search'
@@ -30,9 +31,13 @@ class App extends Component {
     })
   }
   render() {
+    const videoSearch = _.debounce((term) => {
+      this.videSearch(term)
+    }, 300) 
+
     return (
       <div >
-        <SearchBar onSearchTermChange={term => this.videSearch(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo }) }
